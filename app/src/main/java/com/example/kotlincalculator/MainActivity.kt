a103d94d29b09d067192fb82619cac759ff6ad94
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-02-14 10:40:00 +0530

    Add : onClear and onDigit

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.kotlincalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this,"Button works",Toast.LENGTH_SHORT).show()
+        val textViewInput : TextView = findViewById(R.id.textViewInput)
+        textViewInput.append((view as Button).text)
+    }
+
+    fun onClear(view : View){
+        val textViewInput : TextView = findViewById(R.id.textViewInput)
+        textViewInput.text=""
     }
 }
