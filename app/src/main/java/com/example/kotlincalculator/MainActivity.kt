7473010057721bfcc8331cc9cf63515879b32e07
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-02-14 12:53:47 +0530

    Add : All operations

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.ArithmeticException
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +37,91 @@ class MainActivity : AppCompatActivity() {
             textViewInput.append(".")
             lastNumeric = false
             lastDot = true
+        }
+    }
+
+    fun onOperator(view:View){
+        val textViewInput : TextView = findViewById(R.id.textViewInput)
+        if(lastNumeric && !isOperatorAdded(textViewInput.text.toString())){
+            textViewInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    fun onEqual(view : View){
+        val textViewInput : TextView = findViewById(R.id.textViewInput)
+        if(lastNumeric){
+            var textViewValue = textViewInput.text.toString()
+            try {
+                var prefix = ""
+
+                // handling the negative numbers
+                if(textViewValue.startsWith("-")){
+                    prefix="-"
+                    textViewValue = textViewValue.substring(1)
+                }
+
+                // operation to be performed on subtraction
+                if(textViewValue.contains("-")){
+                    val splitValue = textViewValue.split("-")
+                    var firstNumber = splitValue[0]
+                    var secondNumber = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        firstNumber = prefix + firstNumber
+                    }
+
+                    textViewInput.text = (firstNumber.toDouble() - secondNumber.toDouble()).toString()
+                }
+                // operation to be performed on addition
+                if(textViewValue.contains("+")){
+                    val splitValue = textViewValue.split("+")
+                    var firstNumber = splitValue[0]
+                    var secondNumber = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        firstNumber = prefix + firstNumber
+                    }
+
+                    textViewInput.text = (firstNumber.toDouble() + secondNumber.toDouble()).toString()
+                }
+                //operation to be performed on multiply
+                if(textViewValue.contains("*")){
+                    val splitValue = textViewValue.split("*")
+                    var firstNumber = splitValue[0]
+                    var secondNumber = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        firstNumber = prefix + firstNumber
+                    }
+
+                    textViewInput.text = (firstNumber.toDouble() * secondNumber.toDouble()).toString()
+                }
+                //operation to be performed on divide
+                if(textViewValue.contains("/")){
+                    val splitValue = textViewValue.split("/")
+                    var firstNumber = splitValue[0]
+                    var secondNumber = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        firstNumber = prefix + firstNumber
+                    }
+
+                    textViewInput.text = (firstNumber.toDouble() / secondNumber.toDouble()).toString()
+                }
+            }catch (e:ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // function to check is operator added
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if(value.startsWith("-")){
+            return false
+        }else{
+            value.contains("/") || value.contains("+") || value.contains("-") || value.contains("*")
         }
     }
 }
